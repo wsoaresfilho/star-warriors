9e4fad2a74219c459ef50713a311140c5aaa7151
Washington Soares <wsoaresfilho@hotmail.com.br>
2019-11-11 08:46:23 -0300

    Add important message to reviewers

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,10 @@
+// This key is exposed only while the project gets reviewed!
+// It has to be deleted from code as soon as possible.
+// Reviewers: please let me know as soon as you finish testing the app
+const gsak = 'AIzaSyCFhQn97SQxwxgCTSvFHb1aYxaNoc_KXu4';
+const ckk = '002028002876040441306:7gjv47i0lhn';
 const apiBase = 'https://swapi.co/api';
-const googleImgApi =
-    'https://www.googleapis.com/customsearch/v1?key=AIzaSyCFhQn97SQxwxgCTSvFHb1aYxaNoc_KXu4&cx=002028002876040441306:7gjv47i0lhn&searchType=image&imgSize=large&imgType=photo&num=1&q=';
+const googleImgApi = `https://www.googleapis.com/customsearch/v1?key=${gsak}&cx=${ckk}&searchType=image&imgSize=large&imgType=photo&num=1&q=`;
 
 export const getDataFromApi = async url => {
     const response = await fetch(url || `${apiBase}/people/`);
